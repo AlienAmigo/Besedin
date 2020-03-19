ab187df709923d336213ba6284838a298a84e0cd
Pyotr <maestro_pete@inbox.ru>
2020-03-19 09:52:49 +0300

    сделал footer и частично header + main-nav

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,24 @@ function ready(fn) {
 
 ready(function(){
   console.log('DOM ready');
+
+  const burgerMenu = document.querySelector('#menu-burger');
+  const mainNav = document.querySelector('#main-nav');
+  const mainNavCloseBtn = document.querySelector('#main-nav');
+
+  function toggleMenu() {
+    mainNav.classList.toggle('main-nav--mobile-active');
+  }
+
+  if (burgerMenu) {
+    burgerMenu.addEventListener('click', ev => {
+      mainNav.classList.add('main-nav--mobile-active');
+    });
+  }
+
+  if (mainNavCloseBtn) {
+    mainNavCloseBtn.addEventListener('click', ev => {
+      mainNav.classList.remove('main-nav--mobile-active');
+    });
+  }
 });
