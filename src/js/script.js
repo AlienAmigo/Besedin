eb25c1729d7d80172f0ae587870ee936381cab74
Pete <maestro_pete@inbox.ru>
2020-03-28 10:40:01 +0300

    сделал убирающееся меню на мобилке

--- src/js/script.js
+++ src/js/script.js
@@ -9,9 +9,15 @@ function ready(fn) {
 ready(function(){
   console.log('DOM ready');
 
+  // Работа с главным меню
   const burgerMenu = document.querySelector('#menu-burger');
   const mainNav = document.querySelector('#main-nav');
   const mainNavCloseBtn = document.querySelector('#main-nav-close-btn');
+  const mainNavLinks = document.querySelectorAll('.main-nav__link');
+
+  function closeMenu() {
+      mainNav.classList.remove('main-nav--mobile-active');
+  }
 
   function toggleMenu() {
     mainNav.classList.toggle('main-nav--mobile-active');
@@ -23,10 +29,33 @@ ready(function(){
       });
     }
 
-    if (mainNavCloseBtn) {
+  if (mainNavCloseBtn) {
       mainNavCloseBtn.addEventListener('click', ev => {
         toggleMenu();
     });
+  }
+
+  mainNavLinks.forEach((item) => {
+    item.addEventListener('click', ev => {
+      closeMenu();
+    });
+  });
+
+  // Плавная прокрутка
+
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+  for (let item of anchors) {
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const blockID = item.getAttribute('href').substr(1)
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
   }
 
   // NEWS Slider
